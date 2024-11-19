765866174627d8ae4aa3b0a973eae3728e8ae5f8
Angel <xagalzay340@ieshnosmachado.org>
2024-11-19 10:56:20 +0100

    commit

--- app/src/main/java/com/angelgallegozayas/examenEj2/MainActivity.kt
+++ app/src/main/java/com/angelgallegozayas/examenEj2/MainActivity.kt
@@ -40,22 +40,3 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
-
-//@Composable
-//fun navigation(){
-//
-//    val navController = rememberNavController()
-//    //al final ponemos la primera pantalla que queremos que se muestre
-//    NavHost(navController = navController, startDestination = Productos){
-//    composable<Productos> {
-//        ProductosScreen { nombreProducto, precio, imagen -> navController.navigate(dDetalleProducto(nombreProducto, precio, imagen)) } }
-//    composable<dDetalleProducto> { P ->
-//       val detalleProducto = P.toRoute<dDetalleProducto>()
-//        ScreenDetalleProducto(detalleProducto.nombre,
-//            detalleProducto.precio,
-//            detalleProducto.imagen)
-//    }
-//    }
-//
-//
-//}
